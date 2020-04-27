175c30f438accadc64e67042521bfbd7f8ecd8db
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-04-27 17:31:09 +0300

    Style the values

--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -136,6 +136,11 @@ class CryptoCard extends StatelessWidget {
           padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
           child: Text(
             '$cryptoCurrency = $value $selectedCurrency',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 20.0,
+              color: Colors.white,
+            ),
           ),
         ),
       ),
